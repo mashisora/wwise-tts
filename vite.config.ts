cfd9fb8033f3a4ac9f70e87eb45cf3069b92d143
mashisora <mashisora@outlook.com>
2022-07-05 13:40:20 +0000

    init

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { rmSync } from 'fs'
+import { join } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import electron from 'vite-plugin-electron'
+import pkg from './package.json'
+
+rmSync('dist', { recursive: true, force: true }) // v14.14.0
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    electron({
+      main: {
+        entry: 'electron/main/index.ts',
+        vite: {
+          build: {
+            outDir: 'dist/electron/main',
+          },
+        },
+      },
+      preload: {
+        input: {
+          // You can configure multiple preload here
+          index: join(__dirname, 'electron/preload/index.ts'),
+        },
+        vite: {
+          build: {
+            // For debug
+            sourcemap: 'inline',
+            outDir: 'dist/electron/preload',
+          }
+        }
+      },
+      // Enables use of Node.js API in the Renderer-process
+      renderer: {},
+    }),
+  ],
+})
