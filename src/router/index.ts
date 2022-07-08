f68f3e027f01234e1c1a6a956fd932e3768a5730
mashisora <mashisora@outlook.com>
2022-07-08 17:46:51 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 // import Home from '../views/Home.vue'
 // import Settings from '../views/Settngs.vue'
 import HelloWorld from "../views/HelloWorld.vue";
@@ -8,7 +8,7 @@ import Settings from "../views/Settings.vue";
 import routes from "./routes";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
