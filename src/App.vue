e78ec7e020c526f63428ce62bc843a00a89056a5
mashisora <mashisora@outlook.com>
2022-07-09 22:23:52 +0800

    naive-ui fullly treeshaking

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <AppProvider>
-    <n-layout position="absolute">
+    <NLayout position="absolute">
       <Watermark />
-      <n-layout has-sider position="absolute" style="bottom: 22px">
-        <n-layout-sider bordered width="200" :native-scrollbar="false">
+      <NLayout has-sider position="absolute" style="bottom: 22px">
+        <NLayoutSider bordered width="200" :native-scrollbar="false">
           <AppMenu />
-        </n-layout-sider>
-        <n-layout-content content-style="padding: 24px" :native-scrollbar="false">
+        </NLayoutSider>
+        <NLayoutContent content-style="padding: 24px" :native-scrollbar="false">
           <RouterView />
-        </n-layout-content>
-      </n-layout>
-      <n-layout-footer bordered position="absolute" style="height: 22px">
+        </NLayoutContent>
+      </NLayout>
+      <NLayoutFooter bordered position="absolute" style="height: 22px">
         <AppStatus />
-      </n-layout-footer>
-    </n-layout>
+      </NLayoutFooter>
+    </NLayout>
   </AppProvider>
 </template>
 
 <script lang="ts" setup>
+import { NLayout, NLayoutSider, NLayoutContent, NLayoutFooter } from "naive-ui";
 import AppProvider from "./components/AppProvider.vue";
 import AppMenu from "./components/AppMenu.vue";
 import AppStatus from "./components/AppStatus.vue";
